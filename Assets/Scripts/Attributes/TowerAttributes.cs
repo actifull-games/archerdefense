180cf789c74bc98fa910594a9e120344dfeffb37
DirectXManiac <directxmaniac@gmail.com>
2024-02-24 23:49:41 +0300

    Added tower attribute set

---
+++ Assets/Scripts/Attributes/TowerAttributes.cs
@@ -0,0 +1,21 @@
+﻿using MobileFramework.Abilities;
+
+namespace Attributes
+{
+    public class TowerAttributes : GameplayAttributesBase
+    {
+        public GameplayAttribute<float> Damage { get; } = 5.0f;
+        public GameplayAttribute<float> AttackSpeed { get; } = 4.0f;
+        public GameplayAttribute<float> AttackRange { get; } = 10.0f;
+        
+        public override void PreAttributeChange(string attributeName, GameplayAttributeBase attribute, ref bool cancelChange)
+        {
+            cancelChange = false;
+        }
+
+        public override void PostAttributeChange(string attributeName, GameplayAttributeBase attribute)
+        {
+            
+        }
+    }
+}
