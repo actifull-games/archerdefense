408a4224b410b60a548ddc52c42d250e8cdd2e14
DirectXManiac <directxmaniac@gmail.com>
2024-02-27 00:52:14 +0300

    Test gameplay effect logic

---
+++ Assets/Scripts/Effects/EffectTowerApplyDamageMultiplierStatic.cs
@@ -0,0 +1,49 @@
+﻿using Attributes;
+using MobileFramework.Abilities;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Effects
+{
+    public class EffectTowerApplyDamageMultiplierStatic : GameplayEffectBase
+    {
+
+        public EffectTowerApplyDamageMultiplierStatic()
+        {
+            Type = GameplayEffectType.Static;
+        }
+
+        public override string EffectTag => "Tower.Damage.StaticMultiplier";
+        
+        public override float CalculateDuration(GameplayEffectApplyContext effectContext)
+        {
+            return 0.0f;
+        }
+
+        public override float CalculatePeriod(GameplayEffectApplyContext effectApplyContext)
+        {
+            return 0.0f;
+        }
+
+        private float _addedDamage = 0.0f;
+
+        [AttributesChangeMethod(AttributeChangeOn.Apply)]
+        public void ApplyEffectToAttributes(TowerAttributes attributes, GameplayEffectApplyContext effectApplyContext)
+        {
+            attributes.MakeTransactionChanges<TowerAttributes>((t, attr) =>
+            {
+                _addedDamage = Mathf.Pow(1.2f, (float)effectApplyContext.Level);
+                attr.Damage.CurrentValue += _addedDamage;
+            });
+        }
+
+        [AttributesChangeMethod(AttributeChangeOn.Clear)]
+        public void ClearEffectFromAttributes(TowerAttributes attributes, GameplayEffectApplyContext effectApplyContext)
+        {
+            attributes.MakeTransactionChanges<TowerAttributes>((t, attr) =>
+            {
+                attr.Damage.CurrentValue -= _addedDamage;
+            });
+        }
+    }
+}
